52b637eaea88aeaf39140cf45e1c5b0f112e5b0c
joaquingalang <galangjoaquin@gmail.com>
2025-11-06 00:42:28 +0800

    Update: full responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,27 +11,27 @@ function Footer() {
         <div>
             <hr className="border border-light/10 rounded-full mt-[5rem]"/>
             <div className="w-full flex flex-col md:flex-row gap-5 justify-between items-center p-7">
-                <p className="font-poppins text-light/50 text-center">Copyright © 2025 Joaquin Galang. All rights reserved.</p>
+                <p className="font-poppins text-light/50 text-sm md:text-base text-center">Copyright © 2025 Joaquin Galang. All rights reserved.</p>
                 <div className="flex gap-5">
                     <AnimatedIconButton 
                         activeLogo={GitHubLoop} 
                         inactiveLogo={GitHubStatic} 
                         href="https://github.com/joaquingalang"
                         target="_blank"
-                        className="w-[2.5rem] h-[2.5rem]"
+                        className="w-[2rem] md:w-[2.5rem] h-[2rem] md:h-[2.5rem]"
                     />
                     <AnimatedIconButton 
                         activeLogo={LinkedInLoop} 
                         inactiveLogo={LinkedInStatic} 
                         href="https://www.linkedin.com/in/joaquin-galang/"
                         target="_blank"
-                        className="w-[2.5rem] h-[2.5rem]"
+                        className="w-[2rem] md:w-[2.5rem] h-[2rem] md:h-[2.5rem]"
                     />
                     <AnimatedIconButton 
                         activeLogo={EmailLoop} 
                         inactiveLogo={EmailStatic} 
                         href="mailto:galang.joaquin.dev@gmail.com"
-                        className="w-[2.5rem] h-[2.5rem]"
+                        className="w-[2rem] md:w-[2.5rem] h-[2rem] md:h-[2.5rem]"
                     />
                 </div>
             </div>
